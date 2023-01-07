be5447d818d2506bdcd8a57a159e90eb5a5b3cae
John Doe <johndoe@email.com>
2023-01-07 21:07:27 +0500

    new commit

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 
-if (process.env.NODE_ENV == "production") {
-
-    app.use(express.static("client/build"));
-}
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
