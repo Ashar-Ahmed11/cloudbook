aaa4ba486860a19daa0950f6b8c561e69499b7d7
John Doe <johndoe@email.com>
2023-01-07 21:27:10 +0500

    newcommit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ connectToMongo()
 const cors = require('cors')
 const express = require('express')
 const app = express()
-const port = process.env.CYCLIC_URL || 3000;
+const port = process.env.PORT || 3000;
 
 
 
